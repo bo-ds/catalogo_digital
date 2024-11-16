484982ffb2d4ab813be9624c10d0b8b350b7b85d
eu <2211351@aluno.univesp.br>
2024-11-16 16:08:46 -0300

    catalogo_digital

---
+++ script.js
@@ -0,0 +1,24 @@
+const produtos = {
+    id: 1,
+    nome: "Sapateira",
+    categoria: "moveis",
+    preco: 120.00,
+    descricao: "Sapateira em madeira pinus, com acabamento rústico, serve como banco. R$ 120,00.",
+    imagem: "/images/sapateira.jpg"
+},
+{
+    id: 2,
+    nome: "Luminária de chão",
+    categoria: "iluminacao",
+    preco: 80.00,
+    descricao: "Luminária de chão em formato de tripé, construída com madeira de reflorestamento. Disponível nos acabamentos rústico e cru.",
+    imagem: "/images/luminaria_tripe_chao.jpg"
+}
+{
+    id: 3,
+    nome: "Placa decorativa",
+    categoria: "decoracao",
+    preco: 30.00,
+    descricao: "Placa redonda de madeira pinus, com pintura orgânica. R$ 30,00",
+    imagem:"/images/placa_redonda.jpg"
+}
